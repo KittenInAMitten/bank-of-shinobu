e4f4d27d5b9faa3892b973bb999b9db3ddf357c8
Glitch (bank-of-shinobu) <none>
2019-01-09 01:04:45 +0000

    🚕🔑 Updated with Glitch

--- bot/commands.js
+++ bot/commands.js
@@ -10,28 +10,7 @@ module.exports = {
   //test
   
   test: async (message, args, bot) => {
-    //if(!(bot.users.get(args[0].replace(/[<@!>]/g, '')))) return console.log(`not a user!`);
-    //if(bot.users.get(args[0].replace(/[<@!>]/g, ''))) return console.log(`a user!`);
-    
-    /**
-    const cd = require('./data/cooldowns.json');
-    const server = message.guild.id;
-    if(!cd.hasOwnProperty(server)){
-      cd[server] = {};
-    }
-    if(moment().diff(moment(cd[server][message.author.id]), "seconds") < 30){
-      message.channel.send(`30 seconds haven't passed yet \n${moment()}\n${moment(cd[server][message.author.id])}`);
-      return;
-    }
-    message.channel.send(`30 seconds have passed already\n${moment()}\n${moment(cd[server][message.author.id])}`);
-    cd[server][message.author.id] = moment();
-    fs.writeFile('/app/bot/data/cooldowns.json', JSON.stringify(cd), function (err) {
-		if (err) return console.log(err);
-		});
-    **/
-    
-    
-    
+    message.channel.send("Command does nothing.")
   }
 }
 
